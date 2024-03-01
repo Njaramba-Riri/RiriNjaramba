d19dd105f73d8e9caa1248b66a3888568fa15f67
Riri <jamesriri03@gmail.com>
2024-03-01 12:26:04 +0300

    Add comment moderation functionality

---
+++ migrations/versions/4abdffa96ad7_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4abdffa96ad7
+Revises: 
+Create Date: 2024-02-29 12:28:43.082902
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4abdffa96ad7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
